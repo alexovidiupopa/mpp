be48ddba382a7b3291d2a60fc3cd624505d6efb5
Pop-Vlad <vladpop401@gmail.com>
2020-03-09 10:49:45 +0200

    bug fixes

--- mpp/src/main/java/Model/Assignment.java
+++ mpp/src/main/java/Model/Assignment.java
@@ -2,6 +2,8 @@ package Model;
 
 import Utils.Pair;
 
+import java.util.Objects;
+
 public class Assignment extends BaseEntity<Pair<Long, Long>> {
 
     Double grade;
@@ -15,8 +17,35 @@ public class Assignment extends BaseEntity<Pair<Long, Long>> {
         this.grade = grade;
     }
 
+    public void setGrade(Double grade) {
+        this.grade = grade;
+    }
+
     public Double getGrade(){
         return this.grade;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Assignment assignment = (Assignment) o;
+        return  (this.grade == assignment.grade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.getId().getFirst(), super.getId().getSecond(), this.grade);
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                "studentId='" + super.getId().getFirst() + '\'' +
+                ", problemId='" + super.getId().getSecond() + '\'' +
+                ", grade=" + this.grade +
+                "} " + super.toString();
     }
 
 }
